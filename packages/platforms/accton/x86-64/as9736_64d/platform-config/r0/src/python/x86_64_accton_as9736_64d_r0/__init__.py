39396ac5a57677e9bb8a02241f6f97fe23e5c981
Alex Lai <alex_lai@edge-core.com>
2022-10-04 03:04:54 +0000

    [Edgecore][AS9736_64D] eliminated some unused codes 1. removed FANx_DUTY_CYCLE_PERCENTAGE_REG in the enum 2. removed and commented out unused codes in __init__.py
    Signed-off-by: Alex Lai <alex_lai@edge-core.com>

--- packages/platforms/accton/x86-64/as9736_64d/platform-config/r0/src/python/x86_64_accton_as9736_64d_r0/__init__.py
+++ packages/platforms/accton/x86-64/as9736_64d/platform-config/r0/src/python/x86_64_accton_as9736_64d_r0/__init__.py
@@ -42,8 +42,8 @@ class OnlPlatform_x86_64_accton_as9736_64d_r0(OnlPlatformAccton,
             ('as9736_64d_sys_cpld', 0x60, 6),
             ('sys_fpga', 0x60, 17),
             ('as9736_64d_fan', 0x33, 25),
-            ('as9736_64d_pdb_l_cpld', 0x60, 36),
-            ('as9736_64d_pdb_r_cpld', 0x60, 44),
+            #('as9736_64d_pdb_l_cpld', 0x60, 36),
+            #('as9736_64d_pdb_r_cpld', 0x60, 44),
             ('scm_cpld', 0x35, 51),
             #('as9736_64d_udb_cpld1', 0x60, 60), read by pcie
             #('as9736_64d_udb_cpld2', 0x61, 61), read by pcie
@@ -75,48 +75,6 @@ class OnlPlatform_x86_64_accton_as9736_64d_r0(OnlPlatformAccton,
             ('as9736_64d_psu2', 0x51, 41),
             ('acbel_fsh082', 0x59, 41),
          ])
-
-        '''
-        # initialize QSFP port 1~8
-        for port in range(1, 5):
-            self.new_i2c_device('optoe1', 0x50, port+21)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port+21), shell=True)
-
-        self.new_i2c_device('optoe1', 0x50, 27)
-        subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (5, 27), shell=True)
-        self.new_i2c_device('optoe1', 0x50, 26)
-        subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (6, 26), shell=True)
-        self.new_i2c_device('optoe1', 0x50, 29)
-        subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (7, 29), shell=True)
-        self.new_i2c_device('optoe1', 0x50, 28)
-        subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (8, 28), shell=True)
-
-        # initialize QSFP port 9~16
-        for port in range(9, 13):
-            self.new_i2c_device('optoe1', 0x50, port+9)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port+9), shell=True)
-        for port in range(13, 17):
-            self.new_i2c_device('optoe1', 0x50, port+17)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port+17), shell=True)
-
-        # initialize QSFP port 17~24
-        for port in range(17, 21):
-            self.new_i2c_device('optoe1', 0x50, port+17)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port+17), shell=True)
-        for port in range(21, 25):
-            self.new_i2c_device('optoe1', 0x50, port+25)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port+25), shell=True)
-
-        # initialize QSFP port 25~32
-        for port in range(25, 33):
-            self.new_i2c_device('optoe1', 0x50, port+13)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port+13), shell=True)
-
-        # initialize SFP port 33~34
-        for port in range(33, 35):
-            self.new_i2c_device('optoe2', 0x50, port-17)
-            subprocess.call('echo port%d > /sys/bus/i2c/devices/%d-0050/port_name' % (port, port-17), shell=True)
-        '''
 
         self.new_i2c_device('as973d_64d_sys', 0x51, 20)
 
