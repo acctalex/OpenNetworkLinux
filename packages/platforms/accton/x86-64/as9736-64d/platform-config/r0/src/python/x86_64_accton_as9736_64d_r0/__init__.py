6df019dc60e01a7eff64ae05ce3a2912719aefb5
Willy Liu <willy@accton.com>
2025-02-05 08:31:18 +0000

    [ACCTON][AS9736-64D] Remove commands in init.py
    Signed-off-by: Willy Liu <willy@accton.com>

--- packages/platforms/accton/x86-64/as9736-64d/platform-config/r0/src/python/x86_64_accton_as9736_64d_r0/__init__.py
+++ packages/platforms/accton/x86-64/as9736-64d/platform-config/r0/src/python/x86_64_accton_as9736_64d_r0/__init__.py
@@ -1,8 +1,6 @@
 from onl.platform.base import *
 from onl.platform.accton import *
 
-import commands
-
 class OnlPlatform_x86_64_accton_as9736_64d_r0(OnlPlatformAccton,
                                               OnlPlatformPortConfig_64x400_2x10):
 
