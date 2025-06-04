5c2c881ed76d6e48218bcd2fee8914ef9a78434c
Alex Lai <alex_lai@accton.com>
2025-06-04 03:04:16 +0000

    [Accton][as7515-24x] Fix the name identifier of DC PSU CRXT-T0T12 in onlpdump
    Signed-off-by: Alex Lai <alex_lai@accton.com>

--- packages/platforms/accton/x86-64/as7515-24x/onlp/builds/x86_64_accton_as7515_24x/module/src/platform_lib.c
+++ packages/platforms/accton/x86-64/as7515-24x/onlp/builds/x86_64_accton_as7515_24x/module/src/platform_lib.c
@@ -166,10 +166,10 @@ psu_type_t get_psu_type(int id, char* modelname, int modelname_len)
         return PSU_TYPE_SPAACTN_03;
     }
 
-    if (!strncmp(str, "CRXT_T0T12", strlen("CRXT_T0T12")))
+    if (!strncmp(str, "CRXT-T0T12", strlen("CRXT-T0T12")))
     {
         if (modelname)
-            aim_strlcpy(modelname, str, strlen("CRXT_T0T12")<(modelname_len-1)?(strlen("CRXT_T0T12")+1):(modelname_len-1));
+            aim_strlcpy(modelname, str, strlen("CRXT-T0T12")<(modelname_len-1)?(strlen("CRXT-T0T12")+1):(modelname_len-1));
             AIM_FREE_IF_PTR(str);
         return PSU_TYPE_CRXT_T0T12;
     }
